8b0ce244c092dba45a055ce712795c5d7b4b4f0f
Pisces000221 <1786762946@qq.com>
2014-05-09 23:07:39 +0800

    Fix black screen on Android (father-tricking escape...)

--- src/widgets/ScoreLabel.lua
+++ src/widgets/ScoreLabel.lua
@@ -10,7 +10,7 @@ function ScoreLabel.setNumber(self, number)
     --stackoverflow.com/questions/10989788/lua-format-integer
     s = s:reverse():gsub('(%d%d%d)', '%1,'):reverse():gsub('^,', '')
     self:setString(s)
-    local zeroes, ct = string.gsub(s, '([0\,]*).+', '%1')
+    local zeroes, ct = string.gsub(s, '([0\\,]*).+', '%1')
     zeroes = string.len(zeroes)
     for i = 0, zeroes - 1 do self:getLetter(i):setColor(ScoreLabel.zeroColour) end
     for i = zeroes, string.len(s) - 1 do self:getLetter(i):setColor(ScoreLabel.validNumColour) end
